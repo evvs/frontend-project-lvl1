5678bbb829f0fb1e2101a181e500c990d22f80f8
user <user@MSK-M060.brc.local>
2019-07-02 18:17:32 +0300

    from work

---
+++ src/bin/brain-even.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import { greetings } from '..';
+
+console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".')
+
+greetings()
+
--- src/index.js
+++ src/index.js
@@ -2,4 +2,20 @@ import readlineSync from 'readline-sync';
 
 const greetings = (username = readlineSync.question('May I have your name? ')) => console.log(`Hello, ${username}!`);
 
-export { greetings };
+const gameEven = (randomNumber = Math.floor(Math.random() * 100), acc=0) => {
+
+  console.log(`Question: ${randomNumber}`);
+  const userInput = readlineSync.question('Your answer: ');
+
+  //console.log(`Question: ${randomNumber}`);
+
+  if (randomNumber % 2 === 0 && userInput === 'no') {
+       console.log('stupid')
+  }
+  else {
+    console.log('excellent')
+  }
+} 
+
+gameEven()
+export { greetings, gameEven };
