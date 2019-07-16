7bad91340e29bb55798de11ac4e9069c15603a3c
Evgenii <va.evgenii@gmail.com>
2019-07-16 23:38:52 +0300

    code refactoring

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,8 +3,8 @@ import { gameEngine, randomNumber } from '..';
 
 const instruction = 'Find the greatest common divisor of given numbers.';
 
-const findGCD = (a, b) => (!b ? a : findGCD(b, a % b));
+const findGcd = (a, b) => (!b ? a : findGcd(b, a % b));
 
-const gcdGame = (num1 = randomNumber(), num2 = randomNumber()) => cons(`${num1} ${num2}`, `${findGCD(num1, num2)}`);
+const gcdGame = (num1 = randomNumber(), num2 = randomNumber()) => cons(`${num1} ${num2}`, `${findGcd(num1, num2)}`);
 
 export default () => gameEngine(gcdGame, instruction);
